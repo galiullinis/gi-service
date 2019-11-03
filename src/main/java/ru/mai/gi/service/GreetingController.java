39c45f488c892ada025333bef7c5a18baaa7e697
Галиуллин Ильдар <ldrgllln@gmail.com>
2019-11-04 01:28:17 +0400

    some corrections

--- src/main/java/ru/mai/gi/service/GreetingController.java
+++ src/main/java/ru/mai/gi/service/GreetingController.java
@@ -1,11 +1,10 @@
-package hello;
+package ru.mai.gi.service;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.Map;
