31bc1d8983277d866120904cc157c6cced9ca9a2
Галиуллин Ильдар <ldrgllln@gmail.com>
2019-11-04 01:18:57 +0400

    added post request action

---
+++ src/main/java/hello/Content.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.io.*;
+
+public class Content {
+    private String content;
+
+    public String getContent() throws IOException {
+        return content;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public void saveFile() throws IOException {
+        String fileName = "src/main/resources/intermediate.txt";
+        File file = new File(fileName);
+        BufferedWriter bufferedWriter = new BufferedWriter(
+                new FileWriter(file));
+        bufferedWriter.write(content);
+        bufferedWriter.close();
+        file.delete();
+    }
+}
---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @PostMapping("/classify")
+    public String classify(@ModelAttribute Content content, Map<String, Object> model) throws IOException {
+        content.saveFile();
+        model.put("textResult", content.getContent());
+
+        return "result";
+    }
+
+}
